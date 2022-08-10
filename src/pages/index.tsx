a2de32b36ca82b80de2026bc448d80e4a6f883d3
Yudai Kawakami <yudainepnep@icloud.com>
2022-08-10 12:07:17 +0900

    feat: design main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
 import { useMediaQuery, useViewportSize } from "src/lib/mantine";
 
-import { Button, Container } from "@mantine/core";
+import { Button, Container, Text, ThemeIcon, ActionIcon } from "@mantine/core";
+import { IconBrandTwitter, IconBrandFacebook, IconRss } from "@tabler/icons";
 import Layout from "src/component/Layout";
 
 const Home: NextPage = () => {
@@ -18,10 +19,16 @@ const Home: NextPage = () => {
 
   return (
     <Layout title="Home">
-      <div className="w-full bg-red-400 h-60">
+      <div className="w-full bg-m_pink-6 h-60">
         <Container size="md">
-          <div className="bg-pink flex justify-between items-center">
-            <div className="bg-fuchsia-200 xs:bg-red-200 sm:bg-amber-200 md:bg-lime-200 lg:bg-emerald-200 xl:bg-cyan-200">
+          <div
+            className={
+              largerThanSm
+                ? "flex justify-between items-center h-60"
+                : "flex flex-col justify-center h-60"
+            }
+          >
+            {/* <div className="bg-fuchsia-200 xs:bg-red-200 sm:bg-amber-200 md:bg-lime-200 lg:bg-emerald-200 xl:bg-cyan-200">
               <div>{`width: ${width}`}</div>
               <div>{`largerThanXs: ${largerThanXs}`}</div>
               <div>{`largerThanSm: ${largerThanSm}`}</div>
@@ -31,7 +38,29 @@ const Home: NextPage = () => {
             </div>
             <Button onClick={handleClick} className="mt-4 block">
               Click me!
-            </Button>
+            </Button> */}
+
+            {/* 36 16 */}
+            <div>
+              <Text weight={700} size={largerThanSm ? 36 : 28} color="white">
+                Shimabu IT University
+              </Text>
+              <Text weight={700} size="xs" color="white">
+                しまぶーのポートフォリオのためのページです
+              </Text>
+            </div>
+
+            <div className={largerThanSm ? "space-x-2" : "space-x-2 mt-10"}>
+              <ThemeIcon radius="xl" size="md" variant="filled" color="dark">
+                <IconBrandTwitter />
+              </ThemeIcon>
+              <ThemeIcon radius="xl" size="md" variant="filled" color="dark">
+                <IconBrandFacebook />
+              </ThemeIcon>
+              <ThemeIcon radius="xl" size="md" variant="filled" color="dark">
+                <IconRss />
+              </ThemeIcon>
+            </div>
           </div>
         </Container>
       </div>
