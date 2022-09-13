afe4440565095485cc88fc4c739f48a4f9094e33
Yudai Kawakami <yudainepnep@icloud.com>
2022-09-13 19:55:49 +0900

    feat: api route を使ってtweetの取得

--- src/component/Twitter.tsx
+++ src/component/Twitter.tsx
@@ -1,9 +1,44 @@
+import { FC, useCallback, useEffect } from "react";
+import { Button, Text } from "@mantine/core";
+import { Avatar } from "@mantine/core";
+
 import ContentLayout from "src/component/ContentLayout";
 
-const Twitter = () => {
+const Twitter: FC = () => {
+  const getTweets = useCallback(async () => {
+    const data = await fetch("/api/tweets");
+    const json = await data.json();
+  }, []);
+
   return (
     <ContentLayout title="Twitter" buttonTitle="View on Twitter" isHalf>
-      <p>Twitter</p>
+      <div>
+        {[...Array(3)].map((index) => {
+          return (
+            <div className="flex mt-5 mr-2" key={index}>
+              <Avatar />
+              <div>
+                <div className="flex items-center ml-2">
+                  <Text size={16} weight="bold">
+                    しまぶーのIT大学
+                  </Text>
+                  <Text size={12} weight="bold" color="gray" className="ml-2">
+                    shimabu_it・5月25日
+                  </Text>
+                </div>
+                <Text size={12} className="ml-2">
+                  📣 新サービス「Noway Form」をリリースしました！
+                  NowayFormは、Notionのデータベースをもとにフォームを作成できるサービスです。これまでGoogle
+                  FormsでやっていたことがNotionだけで完結します✌✨
+                  試しに使っていただけると幸いです😊
+                  https://www.noway-form.com/ja
+                </Text>
+              </div>
+            </div>
+          );
+        })}
+        <Button onClick={getTweets}>Get Tweet</Button>
+      </div>
     </ContentLayout>
   );
 };
---
+++ src/pages/api/tweets.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Client } from "twitter-api-sdk";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const twitterClient = new Client(process.env.BEARER_TOKEN!);
+  try {
+    const usersTweets = await twitterClient.tweets.usersIdTweets(
+      process.env.TWITTER_USER_ID!
+    );
+    res.status(200).json(usersTweets);
+  } catch {
+    console.log("error");
+    res.status(404);
+  }
+};
+
+export default handler;
